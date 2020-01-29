46e6a8ec42d7c2c891040da7ae36f170d8916575
Aditya Kashyap <adi.inhere@gmail.com>
2020-01-29 16:07:32 +0530

    Modified Build

--- build_project.py
+++ build_project.py
@@ -1,7 +1,7 @@
 import json
 
 # Reading the Build_Project JSON File
-with open('modify_project.json', 'r') as JsonFile:
+with open('build_project.json', 'r') as JsonFile:
     data = json.load(JsonFile)
 
 print("Enter the Project Name")
@@ -10,22 +10,22 @@ data["name"] = input()
 print("Enter the Number of Components you have in the Project that you want to build")
 compo_num = input()
 
-for i in compo_num:
+for i in range(int(compo_num)):
     print("Enter the Component Name")
     comp_name = input()
 
     print("Enter the Branch You want to push it in")
-    comp_branch = input
+    comp_branch = input()
 
     print("Enter the description i.e: trail version")
     comp_desc = input()
 
-    comp = {comp_name: {"branch": comp_branch, "description": comp_desc}}
-    data["components"].append(comp)
+    comp = {"branch": comp_branch, "description": comp_desc}
+    data["components"][comp_name] = comp
 
 # Writing into the JSON File
-with open('build_project.py', 'w') as JsonFile:
-    json.dump(data, JsonFile, indent=1)
+with open('build_project.json', 'w') as JsonFile:
+    json.dump(data, JsonFile, indent=2)
 
 print("Your Build Project :")
 print(json.dumps(data, indent=1))
